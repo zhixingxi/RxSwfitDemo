09c67f3d942c4ad5600c6ef53bf65661ffc53b31
MQL-IT <mantaios@ITdeMacBook-Pro.local>
2018-11-23 14:49:01 +0800

    登录页面

---
+++ RxSwiftDemo/FirstRxSwifController.swift
@@ -0,0 +1,39 @@
+//
+//  FirstRxSwifController.swift
+//  RxSwiftDemo
+//
+//  Created by IT A on 2018/11/23.
+//  Copyright © 2018 IT A. All rights reserved.
+//
+
+import UIKit
+
+class FirstRxSwifController: UIViewController {
+    @IBOutlet weak var tfUsername: UITextField!
+    
+    @IBOutlet weak var tfPassword: UITextField!
+    
+    @IBOutlet weak var lbUsernameTip: UILabel!
+    
+    @IBOutlet weak var lbPasswordTip: UILabel!
+    
+    @IBOutlet weak var btLogin: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
