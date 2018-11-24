b89542027fc65913d3c174ee0923a85e4f646bad
孟庆岭 <>
2018-11-24 20:08:09 +0800

    登录逻辑

--- RxSwiftDemo/FirstRxSwifController.swift
+++ RxSwiftDemo/FirstRxSwifController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class FirstRxSwifController: UIViewController {
     @IBOutlet weak var tfUsername: UITextField!
@@ -19,21 +21,54 @@ class FirstRxSwifController: UIViewController {
     
     @IBOutlet weak var btLogin: UIButton!
     
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+    
+        let usernameValid = tfUsername.rx.text.orEmpty
+            .map { $0.count >= 5 }
+            .share(replay: 1)
         
+        let passwordValid = tfPassword.rx.text.orEmpty
+            .map { $0.count >= 6 }
+            .share(replay: 1)
+        
+        let allValid = Observable.combineLatest(
+            usernameValid,
+            passwordValid)
+            .map{ $0 && $1}
+            .share(replay: 1)
+        
+        usernameValid.bind(to: lbUsernameTip.rx.isHidden)
+            .disposed(by: disposeBag)
+        
+        usernameValid.bind(to: tfPassword.rx.isEnabled)
+            .disposed(by: disposeBag)
+        
+        passwordValid.bind(to: lbPasswordTip.rx.isHidden)
+            .disposed(by: disposeBag)
+        
+        allValid.bind(to: btLogin.rx.isEnabled)
+            .disposed(by: disposeBag)
+        
+        allValid
+            .map { return $0 ? UIColor.green : UIColor.gray }
+            .subscribe(onNext: {[weak self] (color) in
+                self?.btLogin.backgroundColor = color
+            })
+            .disposed(by: disposeBag)
+        
+        btLogin.rx.tap.subscribe(onNext: { [weak self] in
+            self?.showAlert()
+        }).disposed(by: disposeBag)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func showAlert() {
+        let alertVC = UIAlertController(title: "RxExample", message: "This is wonderful", preferredStyle: .alert)
+        let sureAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertVC.addAction(sureAction)
+        self.present(alertVC, animated: true, completion: nil)
     }
-    */
 
 }
